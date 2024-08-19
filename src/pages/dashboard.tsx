6953c426a179bbb326c03093e964142960f229b6
jamshidjanibekov <jamshidjanibekov0@gmail.com>
2024-08-19 14:12:44 +0500

    Dashboard ui

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,45 @@
+import {Button} from "@/components/ui/button.tsx";
+import {BadgePlus} from "lucide-react";
+import {Separator} from "@/components/ui/separator.tsx";
+import TaskItem from "@/components/shared/task-item.tsx";
 
 const Dashboard = () => {
   return (
-    <div className='w-full h-screen flex justify-center items-center '>
-      Dashboard
+    <div className='h-screen max-w-6xl mx-auto flex items-center '>
+      <div className='grid grid-cols-2 w-full gap-8 items-center'>
+        <div className='flex flex-col space-y-3'>
+          <div className='w-full p-4 rounded-md flex justify-between bg-gradient-to-t from-background to-secondary'>
+            <div className='text-2xl font-bold'>
+              Training
+            </div>
+            <Button size='icon'>
+              <BadgePlus/>
+            </Button>
+          </div>
+          <Separator/>
+          <div className='w-full p-4 rounded-md flex justify-between bg-gradient-to-b from-background to-secondary relative min-h-60'>
+            <div className='flex flex-col space-y-3 w-full'>
+              {Array.from({length:3}).map((_, index) => (
+                <TaskItem/>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col space-y-3 relative w-full'>
+          <div className='p-4 rounded-md bg-gradient-to-r from-blue-500 to-background relative h-24'>
+            <div className='text-2xl font-bold'>Total Week</div>
+            <div className='text-3xl font-bold'>20:12:00</div>
+          </div>
+          <div className='p-4 rounded-md bg-gradient-to-r from-secondary to-background relative h-24'>
+            <div className='text-2xl font-bold'>Total Week</div>
+            <div className='text-3xl font-bold'>20:12:00</div>
+          </div>
+          <div className='p-4 rounded-md bg-gradient-to-r from-destructive to-background relative h-24'>
+            <div className='text-2xl font-bold'>Total Week</div>
+            <div className='text-3xl font-bold'>20:12:00</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
